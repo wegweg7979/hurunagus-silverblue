f4223e0f1eb408b0cc4adfabb2cba75581d3edd0
wegweg7979 <diode-crumble-0h@icloud.com>
2026-08-02 15:38:01 +0100

    use gdm instead of greetd

--- files/scripts/bpfland.sh
+++ files/scripts/bpfland.sh
@@ -7,5 +7,5 @@ set -oue pipefail
 
 # Your code goes here.
 #systemctl enable --now scx_loader.service
-systemctl disable gdm
-systemctl enable greetd
+#systemctl disable gdm
+#systemctl enable greetd
